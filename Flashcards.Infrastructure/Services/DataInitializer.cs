2e81d59bbc6713ce8617bbeeda726583360f5c34
PatiDev95 <kubinskapatrycja@gmail.com>
2020-06-09 09:32:30 +0200

    Data Initializer

--- Flashcards.Infrastructure/Services/DataInitializer.cs
+++ Flashcards.Infrastructure/Services/DataInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Flashcards.Infrastructure.IRepositories;
 using Flashcards.Infrastructure.IServices;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Flashcards.Infrastructure.Services
@@ -24,22 +25,24 @@ namespace Flashcards.Infrastructure.Services
         {
             var userId = Guid.NewGuid();
             await _userService.RegisterAsync(userId, "user@email.com", "default user", "secret");
+            var tasks = new List<Task>();
 
-            Guid English = await _categoryService.CreateAsync("English");
-            Guid Biology = await _categoryService.CreateAsync("Biology");
-            Guid Physics = await _categoryService.CreateAsync("Physics");
+            Guid English = await _categoryService.CreateAsync("English", userId);
+            Guid Biology = await _categoryService.CreateAsync("Biology", userId);
+            Guid Physics = await _categoryService.CreateAsync("Physics", userId);
 
-            await _flashcardService.CreateAsync("pasjonujący", "thrilling", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("świadomy", "aware", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("poprzedzać", "to precede", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("omawiać", "to cover", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("szeroki", "wide", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("wąski", "narrow", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("zarządzać", "to manage", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("dudy", "bagpipes", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("cały i zdrowy", "safe and sound", English, Guid.NewGuid(), userId);
-            await _flashcardService.CreateAsync("bardzo szybko, w oka mgnieniu", "in no time (at all)", English, Guid.NewGuid(), userId);
-  
+            tasks.Add(_flashcardService.CreateAsync("pasjonujący", "thrilling", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("świadomy", "aware", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("poprzedzać", "to precede", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("omawiać", "to cover", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("szeroki", "wide", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("wąski", "narrow", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("zarządzać", "to manage", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("dudy", "bagpipes", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("cały i zdrowy", "safe and sound", English, Guid.NewGuid(), userId));
+            tasks.Add(_flashcardService.CreateAsync("bardzo szybko, w oka mgnieniu", "in no time (at all)", English, Guid.NewGuid(), userId));
+
+            await Task.WhenAll(tasks);
         }
     }
 }
